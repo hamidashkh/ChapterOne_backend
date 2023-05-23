d2514b85663bf3aef9ca5a16c85488e4cb35c17e
hamidashkh <hamidashkh@code.edu.az>
2023-05-23 17:12:14 +0400

    controllers

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterOne.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
